15e9a5f2574f0a9228d5922acd55d93fd3026ce6
alexandro.jesus <alexandro.jesus@saude.se.gov.br>
2022-11-29 13:08:43 -0300

    revisao de callback

---
+++ esnext/usandoCallback.js
@@ -0,0 +1,7 @@
+const getTurma = (letra,callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    HTMLOutputElement.get(url, res =>{
+        let resultado = ' '
+        
+    })
+}
